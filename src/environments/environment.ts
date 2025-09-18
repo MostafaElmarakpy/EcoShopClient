4ed03cf1025eb642e61847f39b52ee09db829b26
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-19 00:05:09 +0300

    update  Angular Settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
-    
+    // apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://e-commerce-serverside.vercel.app',
+    baseUrlProducts : 'https://fakestoreapi.in/api/products'
+
 };
