7204a7699da997e1c136af51b143312ed912483d
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-18 23:59:47 +0300

    Update Auth Layout

---
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [RouterOutlet,HeaderComponent , FooterComponent],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.css'
+})
+export class AuthLayoutComponent {
+
+}
