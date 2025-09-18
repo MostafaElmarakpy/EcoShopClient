3068d5abfdff842b1c3a7c7b909f41489a7f1a4c
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-18 23:48:34 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,49 +6,52 @@ import { ProductsComponent } from './pages/product/products/products.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { authGuard } from './guards/auth-guard';
+
+
+
 export const routes: Routes = [
-    {
+  {
     path: '',
-    loadComponent: () =>
-      import('./layouts/auth-layout/auth-layout.component').then(
-        (c) => c.AuthLayoutComponent
-      ),
+    loadComponent: () => import('./layouts/auth-layout/auth-layout.component').then(c => c.AuthLayoutComponent),
     children: [
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       {
-        path: 'register',
-        loadComponent: () =>
-          import('./pages/signup/signup.component').then(
-            (c) => c.SignupComponent
-          ),
-        canDeactivate: [authGuard],
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(c => c.LoginComponent),
+        // Public: No guard
       },
       {
-        path: 'login',
-        loadComponent: () =>
-          import('./pages/login/login.component').then((c) => c.LoginComponent),
+        path: 'register',  // Or 'signup'
+        loadComponent: () => import('./pages/signup/signup.component').then(c => c.SignupComponent),
+        // Public: No guard (changed from canDeactivate)
       },
     ],
-    },
-    {
+  },
+  {
     path: 'home',
-    loadComponent: () =>
-        import('./pages/home/home.component').then((c) => c.HomeComponent),
-    },
-    {
-    path: 'products',
-    loadComponent: () =>
-        import('./pages/product/products/products.component').then(
-        (c) => c.ProductsComponent
-        ),
-    },
-
-
-    { path: 'about', component: AboutComponent },
-    { path: 'contact', component: ContactComponent },
-     { path: 'product/:id', component: ProductsComponent },
-   
-
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+    // canActivate: [authGuard],  // Protected
+  },
+  {
+    path: 'products',  // List
+    loadComponent: () => import('./pages/product/products/products.component').then(c => c.ProductsComponent),
+    // canActivate: [authسشئيريبلا Guard],
+  },
+  {
+    path: 'products/:id',  // Details (see below)
+    loadComponent: () => import('./pages/product/detail-product/detail-product.component').then(c => c.DetailProductComponent),
+    // canActivate: [authGuard],
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./components/about/about.component').then(c => c.AboutComponent),
+    // Public
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./components/contact/contact.component').then(c => c.ContactComponent),
+    // Public
+  },
 
     { path: '**', redirectTo: 'home' },
 ];
