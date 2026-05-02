18caeabe41d115c12a4b69bbb5d04270d6320e35
MOSTAFA <MOSTAFA@DESKTOP-2G14B1N>
2026-05-02 07:02:22 +0300

    update routing

--- src/app/pages/product/products-routing.module.ts
+++ src/app/pages/product/products-routing.module.ts
@@ -1,6 +1,5 @@
 import { RouterModule,Routes } from "@angular/router";
 import { ProductsComponent } from "./products/products.component";
-import { UpdateProductComponent } from "./update-product/update-product.component";
 import { NgModule } from "@angular/core";
 // import { DetailProductComponent } from "./detail-product/detail-product.component";
 
@@ -9,7 +8,6 @@ const routes: Routes = [
     { path: '', component: ProductsComponent },
     // { path: 'product/:id' },
     { path: 'product', component: ProductsComponent },
-      { path: 'update/:id', component: UpdateProductComponent },
       
 
     { path: '**', redirectTo: 'product' },
--- src/app/pages/product/products.module.ts
+++ src/app/pages/product/products.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 import { ConfirmModalComponent } from "../../components/confirm-modal/confirm-modal.component";
-import { UpdateProductComponent } from "./update-product/update-product.component";
 import { ProductsRoutingModule } from "./products-routing.module";
 // import { DetailProductComponent } from "./detail-product/detail-product.component";
 
@@ -11,11 +10,10 @@ import { ProductsRoutingModule } from "./products-routing.module";
 @NgModule({
   declarations: [
   // DeleteProductComponent,
-  UpdateProductComponent,
-    ConfirmModalComponent,  
+
   ],
   imports: [CommonModule, ProductsRoutingModule,ReactiveFormsModule],
-  exports: [ConfirmModalComponent],
+  // exports: [ConfirmModalComponent],
 })
 export class ProductsModule { }
 
